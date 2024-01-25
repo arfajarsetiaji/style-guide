cc5b29ba3a7c0fc9e4a4342a606cc49229f8b393
Ar Fajar Setiaji <arfajarsetiaji.id@gmail.com>
2024-01-25 19:01:29 +0700

    feat(prettier): add Prettier config for React

---
+++ prettier.config.react.cjs
@@ -0,0 +1,37 @@
+const options = {
+	$schema: 'http://json.schemastore.org/prettierrc',
+	arrowParens: 'avoid',
+	singleQuote: true,
+	useTabs: true,
+};
+
+const plugins = [
+	'@ianvs/prettier-plugin-sort-imports',
+	'prettier-plugin-packagejson',
+	'prettier-plugin-tailwindcss',
+];
+
+const sortImportOptions = {
+	importOrder: [
+		'<BUILTIN_MODULES>',
+		'^(angular$)|^(@angular/(.*)$|^(angular/(.*)$)',
+		'^(react$)|^(@react/(.*)$|^(react/(.*)$)',
+		'^(vue$)|^(@vue/(.*)$|^(vue/(.*)$)',
+		'^(next$)|^(@next/(.*)$)|^(next/(.*)$)',
+		'^(nuxt$)|^(@nuxt/(.*)$)|^(nuxt/(.*)$)',
+		'<THIRD_PARTY_MODULES>',
+		'<TYPES>',
+		'',
+		'^(@)',
+		'',
+		'^[./]',
+	],
+	importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+	importOrderTypeScriptVersion: '5.0.0',
+};
+
+module.exports = {
+	plugins,
+	...options,
+	...sortImportOptions,
+};
